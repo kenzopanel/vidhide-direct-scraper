33519e8f93463408443ab43b8ec06c5a485a1a79
M. Zimam A <pankiller257@gmail.com>
2024-03-08 19:06:10 +0700

    update: browser args

--- app.js
+++ app.js
@@ -41,7 +41,35 @@ function randomInt(min, max) {
 const getDirectLink = async (url) => {
   const browser = await puppeteerExtra.launch({
     headless: true,
-    args: ['--no-sandbox', '--ignore-certificate-errors']
+    args: [
+      '--disable-web-security',
+      '--disable-features=IsolateOrigins,site-per-process',
+      '--allow-running-insecure-content',
+      '--disable-blink-features=AutomationControlled',
+      '--no-sandbox',
+      '--mute-audio',
+      '--no-zygote',
+      '--no-xshm',
+      '--no-first-run',
+      '--no-default-browser-check',
+      '--disable-dev-shm-usage',
+      '--disable-gpu',
+      '--enable-webgl',
+      '--ignore-certificate-errors',
+      '--lang=en-US,en;q=0.9',
+      '--password-store=basic',
+      '--disable-gpu-sandbox',
+      '--disable-software-rasterizer',
+      '--disable-background-timer-throttling',
+      '--disable-backgrounding-occluded-windows',
+      '--disable-renderer-backgrounding',
+      '--disable-infobars',
+      '--disable-breakpad',
+      '--disable-canvas-aa',
+      '--disable-2d-canvas-clip-aa',
+      '--disable-gl-drawing-for-tests',
+      '--enable-low-end-device-mode',
+    ]
   });
 
   const page = await browser.newPage();
