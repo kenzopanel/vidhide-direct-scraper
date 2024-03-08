e694abf15420a699001aaa2665c23621d3c81a40
M. Zimam A <pankiller257@gmail.com>
2024-03-08 17:32:54 +0700

    Fix proxy server

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require("express");
 const app = express();
 const puppeteerExtra = require('puppeteer-extra');
 const Stealth = require('puppeteer-extra-plugin-stealth');
+const proxyChain = require('proxy-chain');
 
 puppeteerExtra.use(Stealth());
 
@@ -31,7 +32,9 @@ app.listen(PORT, () => {
 
 const getDirectLink = async (url) => {
   process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
-  const proxyServer = 'http://paorsnok-rotate:3gelpkdyzj63@p.webshare.io:80';
+  let proxyServer = 'http://paorsnok-rotate:3gelpkdyzj63@p.webshare.io:80';
+  proxyServer = await proxyChain.anonymizeProxy(proxyServer);
+
   const browser = await puppeteerExtra.launch({
     headless: true,
     args: ['--no-sandbox', '--ignore-certificate-errors', `--proxy-server=${proxyServer}`]
